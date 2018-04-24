0eaa97a30ee1d5fa9a3cbf2c8b59640644e8f040
Eric Loew <ericloew@hotmail.com>
2018-04-24 12:27:27 -0500

    iOS

---
+++ ios/AuthNative/FacebookUtil.h
@@ -0,0 +1,13 @@
+//
+//  FacebookUtil.h
+//  AuthNative
+//
+//  Created by Eric Loew on 4/24/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#ifndef FacebookUtil_h
+#define FacebookUtil_h
+
+
+#endif /* FacebookUtil_h */
---
+++ ios/AuthNative/FacebookUtil.m
@@ -0,0 +1,9 @@
+//
+//  FacebookUtil.m
+//  AuthNative
+//
+//  Created by Eric Loew on 4/24/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
